7cb7eaf8b3ae717a241afdc39609612b23c4bdb2
Peter Raswono <email": null>
2016-09-21 19:21:14 +0700

    Peter

--- models/index.js
+++ models/index.js
@@ -8,10 +8,18 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// production
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: true
+  });
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// local
+  if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 }
 
 fs
