0e09664e3ba269c65f3e7f1ae2ec6b7da7d53ea8
Andrew <atandiawan@gmail.com>
2016-09-21 17:09:11 +0700

    test

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.listen(3000)
 app.set('view-engine', 'ejs')
 
-app.get('/', function(req,res,next){
-  model.url.findAll().then(function(urls){
-    if(urls){
-      res.render('./index.ejs',{word: urls})
-    } else {
-      res.render('./index.ejs')
-    }
-  })
+app.get('/',function(req,res,next){
+  res.send('test')
 })
+// app.get('/', function(req,res,next){
+//   model.url.findAll().then(function(urls){
+//     if(urls){
+//       res.render('./index.ejs',{word: urls})
+//     } else {
+//       res.render('./index.ejs')
+//     }
+//   })
+// })
 
 app.post('/urls', function(req,res,next){
   model.url.create({name: req.body.url, click_count:0}).then(function(){
